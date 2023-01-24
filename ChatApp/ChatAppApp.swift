f334beb0cebcdfd49cbf307854e96d86a1082ca3
Ayman Jibrael <aymanjibrael@outlook.com>
2023-01-24 15:21:29 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Ayman on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
